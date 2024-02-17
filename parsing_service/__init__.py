24103faa03946d312843ec1cdc7695b7d6901e27
Alessio <alessio.vertemati@gmail.com>
2024-02-17 10:27:18 +0100

    Improve exception and validation handling

--- parsing_service/__init__.py
+++ parsing_service/__init__.py
@@ -3,6 +3,8 @@ import logging
 import os
 
 import requests
+from requests.exceptions import HTTPError
+from requests.exceptions import Timeout
 from flask import Flask, request
 
 from parsing_service.implementation.parser_factory import parse_file
@@ -22,7 +24,9 @@ def create_app():
 
     @app.route("/extract-text", methods=["POST"])
     def text_extract_endpoint():
+
         logger.info("Received parse request")
+
         if not request.json:
             logger.warning("No json found in request")
             return {"message": "No json found in request", "code": 422, "type": "Unprocessable Entity"}, 422
@@ -32,25 +36,45 @@ def create_app():
         if not request.json.get("mime_type"):
             logger.warning("No mime_type found in request")
             return {"message": "No mime_type found in request", "code": 422, "type": "Unprocessable Entity"}, 422
+        
+        if request.json.get("mime_type") != 'application/pdf':
+            mime = request.json.get("mime_type")
+            logger.warning(f"Unsupported format [{mime}]")
+            return {"message": f"Unsupported mime type. Expecting application/pdf received [{mime}]", "code": 422, "type": "Unprocessable Entity"}, 422
+        
         filename = hashlib.sha256(request.json.get("url").encode()).hexdigest()
         extension = request.json.get("mime_type").split("/")[-1]
         filename = f"{filename}.{extension}"
+        logger.info(f"Parsing {filename}")
+        
         try:
-            resp = requests.get(request.json.get("url"), allow_redirects=True)
+            resp = requests.get(request.json.get("url"), allow_redirects=True, timeout=120)
+
+            resp.raise_for_status()
+
             open(os.path.join(app.resource_path, filename), 'wb').write(resp.content)
-        except Exception:
-            logger.exception("Error while saving file")
+        except HTTPError as http_err:
+            logger.exception("Error while downloading file", exc_info=True)
+            return {"message": f"Error while downloading file [{http_err}]", "code": 500, "type": "Internal Server Error"}, 500
+        except Timeout as http_timeout:
+            logger.exception("Timeout while downloading file", exc_info=True)
+            return {"message": f"File download not completed [{http_timeout}]", "code": 500, "type": "Internal Server Error"}, 500
+        except Exception as requestError:
+            logger.exception("Error while downloading file", exc_info=True)
             return {"message": "Error while saving file", "code": 500, "type": "Internal Server Error"}, 500
+        
         try:
             doc_parsed = parse_file(os.path.join(app.resource_path, filename), extension)
             os.remove(os.path.join(app.resource_path, filename))
-        except ValueError:
-            logger.exception("Unsupported file type")
+        except ValueError as ve:
+            logger.exception("Unsupported file type", exc_info=True)
             return {"message": "Unsupported file type", "code": 422, "type": "Unprocessable Entity"}, 422
-        except Exception:
-            logger.exception("Error while parsing file")
+        except Exception as err:
+            logger.exception("Error while parsing file", exc_info=True)
             return {"message": "Error while parsing file", "code": 500, "type": "Internal Server Error"}, 500
+        
         logger.info(f"Parse done for file {filename}")
+        
         return {"status": "ok", "content": [chunk.to_dict() for chunk in doc_parsed]}, 200
 
     return app
